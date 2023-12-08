dff8466669652efd0c893f92e7a63f68aaeabb2f
deaamanii <dm68549@ubt-uni.net>
2023-12-08 23:53:37 +0100

    Perditesobva pjesen e main tek Contact Us ne hmtl, poashtu disa ndryshime tek pjesa e js per shkak te validimit

--- contactus.js
+++ contactus.js
@@ -1,3 +1,16 @@
 function myFunction() {
     document.getElementById("myForm").reset();
 }
+
+function validateForm() {
+    var name = document.getElementById("emri").value;
+    var email = document.getElementById("email").value;
+    var title = document.getElementById("titulli").value;
+    var message = document.getElementById("mesazhi").value;
+
+    if (name.trim() === "" || email.trim() === "" || title.trim() === "" || message.trim() === "") {
+        alert("Please fill in all required fields.");
+        return false;
+    }
+    return true;
+}
